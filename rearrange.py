df547c28910faca69f55d28f5bc875aadc39c47e
Gagan Malvi <malvigagan@gmail.com>
2022-02-01 23:40:05 +0530

    refactor(round-1): Add .lin and .mac extensions.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- rearrange.py
+++ rearrange.py
@@ -2,8 +2,7 @@
 
 import os
 
-directories = sorted(os.listdir("test"))
-directories.remove(".git")
+directories = sorted(os.listdir("round-1"))
 
 copyDirs = directories
 
@@ -13,25 +12,17 @@ platforms = sorted([
     'linux'
 ])
 
-problemDirs = []
-for i in range(20):
-    problemDirs.append("Problem-" + str(i + 1))
-
 for n in range(20):
     for i in directories: 
-        if not os.path.exists('test/' + str(n + 1)):
-            sourcepathdir = 'test/' + i
-            destpathdir = 'test/' + str(n + 1)
-            os.rename(sourcepathdir, destpathdir)
+            destpathdir = 'round-1/' + i
             for f in platforms:
-                if (f == 'windows'):
-                    srcexec = destpathdir + '/executables/' + i + '-windows.exe'
-                    destexec = destpathdir + '/executables/' + str(n + 1) + '-windows.exe'
-                    os.rename(srcexec, destexec)
-                elif (f == 'linux' or f == 'mac'):
+                if (f == 'linux'):
                     srcexec = destpathdir + '/executables/' + i + '-' + f
-                    destexec = destpathdir + '/executables/' + str(n + 1) + '-' + f
+                    destexec = destpathdir + '/executables/' + i + '-' + f + ".lin"
+                    print(destexec)
+                    os.rename(srcexec, destexec)
+                elif (f == 'mac'):
+                    srcexec = destpathdir + '/executables/' + i + '-' + f
+                    destexec = destpathdir + '/executables/' + i + '-' + f + ".mac"
                     os.rename(srcexec, destexec)
             directories.remove(i)
-        else:
-            break
