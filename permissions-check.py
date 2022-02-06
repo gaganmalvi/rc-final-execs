dde45b1cc4912c30bc91a4fd221820fa69b066c5
Gagan Malvi <malvigagan@gmail.com>
2022-02-06 13:52:09 +0530

    welp, here goes round 2
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- permissions-check.py
+++ permissions-check.py
@@ -3,27 +3,24 @@
 import os
 
 root_directory = "rounds/final-executables/proprietary/"
-dirsRound1 = os.listdir(root_directory + "round-1")
-dirsRound2Easy = os.listdir(root_directory + "round-2/easy")
-dirsRound2Hard = os.listdir(root_directory + "round-2/hard")
-dirsRound2Medium = os.listdir(root_directory + "round-2/medium")
+dirsRound2Easy = os.listdir(root_directory + "round-2")
 
 def changePermsToExec(path):
     os.system("chmod a+x " + path)
     return True
-
+'''
 for directories in dirsRound1:
     changePermsToExec(root_directory + "round-1/" + directories + "/executables/" + directories + "-linux.lin")
     changePermsToExec(root_directory + "round-1/" + directories + "/executables/" + directories + "-mac.mac")
-
+'''
 for directories in dirsRound2Easy:
-    changePermsToExec(root_directory + "round-2/easy/" + directories + "/executables/" + directories + "-linux.lin")
-    changePermsToExec(root_directory + "round-2/easy/" + directories + "/executables/" + directories + "-mac.mac")
+    changePermsToExec(root_directory + "round-2/" + directories + "/executables/" + directories + "-linux.lin")
+    changePermsToExec(root_directory + "round-2/" + directories + "/executables/" + directories + "-mac.mac")
 
-for directories in dirsRound2Hard:
-    changePermsToExec(root_directory + "round-2/hard/" + directories + "/executables/" + directories + "-linux.lin")
-    changePermsToExec(root_directory + "round-2/hard/" + directories + "/executables/" + directories + "-mac.mac")
+#for directories in dirsRound2Easy:
+ #   changePermsToExec(root_directory + "round-2/Easy/" + directories + "/executables/" + directories + "-linux.lin")
+  #  changePermsToExec(root_directory + "round-2/hard/" + directories + "/executables/" + directories + "-mac.mac")
 
-for directories in dirsRound2Medium:
-    changePermsToExec(root_directory + "round-2/medium/" + directories + "/executables/" + directories + "-linux.lin")
-    changePermsToExec(root_directory + "round-2/medium/" + directories + "/executables/" + directories + "-mac.mac")
+#for directories in dirsRound2Medium:
+ #   changePermsToExec(root_directory + "round-2/medium/" + directories + "/executables/" + directories + "-linux.lin")
+  #  changePermsToExec(root_directory + "round-2/medium/" + directories + "/executables/" + directories + "-mac.mac")
